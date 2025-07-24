35b89588e08ab2afd98d465115c0115de4c15871
Suborno22 <subornodas251@gmail.com>
2025-07-24 15:25:25 +0530

    modifying (2)

--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://laravel-app-subdw.onrender.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
